a27063da0756637dc4175838f865524f79025bda
Alexandre Benoit <amgb20@bath.ac.uk>
2023-07-02 17:47:03 +0100

    modification working

--- RL - Q-Learning/sampling_point.py
+++ RL - Q-Learning/sampling_point.py
@@ -1,58 +1,54 @@
-# Base Data Science snippet
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import os
 import time
-from tqdm import tqdm_notebook
-from scipy.spatial.distance import cdist
 import imageio
-from matplotlib.patches import Rectangle
-from matplotlib.collections import PatchCollection
-
-plt.style.use("seaborn-dark")
-
 import sys
-sys.path.append("../")
+
+from tqdm import tqdm_notebook
 from q_agent import QAgent
 
-class SamplingPointEnvironment(object):
-    def __init__(self,n_stops = None,max_box = None, method = "distance", points = None, **kwargs):
+plt.style.use("seaborn-dark")
+sys.path.append("../")
 
-        print(f"Initialized Sampling Point Environment with {n_stops} random stops")
+class SamplingPointEnvironment(object):
+    def __init__(self,stop_count = None,max_limit = None, points = None):
+
+        print(f"Initialized Sampling Point Environment with {stop_count} random visited_stops")
 
         # Initialization
 
         # added
         self.points = points
-        self.n_stops = n_stops
-        self.action_space = self.n_stops # action space is the number of stops
-        self.observation_space = self.n_stops # observation space is the number of stops as well
-        self.max_box = max_box
-        self.stops = [] # list of stops that the agent has visited
-        self.method = method
+        self.stop_count = stop_count
+        self.action_space = self.stop_count # action space is the number of visited_stops
+        self.observation_space = self.stop_count # observation space is the number of visited_stops as well
+        self.max_limit = max_limit
+        self.visited_stops = [] # list of visited_stops that the agent has visited
 
-        # Generate stops
-        self.Generate_Stops()
-        self.Generate_Distance_Matrix()
+
+        # Generate visited_stops
+        self.Create_visited_stops()
+        self.Create_Distance_Matrix()
         self.Render()
 
         # Initialize first point
-        self.Reset()
+        self.Initialise()
 
-    # genereate the coordinates of the stops
-    def Generate_Stops(self):
-        xy = np.array(self.points)
-        self.x = xy[:, 0]
-        self.y = xy[:, 1]
+    # genereate the coordinates of the visited_stops
+    def Create_visited_stops(self):
+        coordinates = np.array(self.points)
+        self.x_coordinates = coordinates[:, 0] # first column of the array being the x coordinates
+        self.y_coordinates = coordinates[:, 1] # second column of the array being the y coordinates
 
-    # generate the distance matrix between the stops using the euclidean distance
-    def Generate_Distance_Matrix(self):
-        self.q_stops = np.zeros((self.n_stops, self.n_stops))
-        for i in range(self.n_stops):
-            for j in range(self.n_stops):
-                self.q_stops[i][j] = np.sqrt((self.x[i] - self.x[j])**2 + (self.y[i] - self.y[j])**2)
-    
+    # generate the distance matrix between the visited_stops using the euclidean distance
+    # the use of cdsit could have been used as well
+    def Create_Distance_Matrix(self):
+        self.stop_distances = np.zeros((self.stop_count, self.stop_count))
+        for i in range(self.stop_count):
+            for j in range(self.stop_count):
+                self.stop_distances[i][j] = np.sqrt((self.x_coordinates[i] - self.x_coordinates[j])**2 + (self.y_coordinates[i] - self.y_coordinates[j])**2)
+
     # Drawing the path of the agent
     def Render(self, return_img=False):
         fig = plt.figure(figsize=(7, 7))
@@ -60,20 +56,20 @@ class SamplingPointEnvironment(object):
         ax.set_title("Delivery Stops")
 
         # Swap x and y when plotting and invert y (which is plotted on x-axis)
-        ax.scatter(self.y, -self.x, c="red", s=50)
+        ax.scatter(self.y_coordinates, -self.x_coordinates, c="red", s=50)
 
-        if len(self.stops) > 0:
+        if len(self.visited_stops) > 0:
             # Swap x and y when getting coordinates and invert y
-            xy = [self.Get_XY(initial=True)[1], -self.Get_XY(initial=True)[0]]
-            xytext = xy[0] - 0.05, xy[1] - 0.1
-            ax.annotate("START", xy=xy, xytext=xytext, weight="bold")
+            coordinates = [self.Fetch_Coordinates(initial=True)[1], -self.Fetch_Coordinates(initial=True)[0]]
+            xytext = coordinates[0] - 0.05, coordinates[1] - 0.1
+            ax.annotate("START", xy=coordinates, xytext=xytext, weight="bold")
 
-        if len(self.stops) > 1:
+        if len(self.visited_stops) > 1:
             # Swap x and y when plotting and invert y
-            ax.plot(self.y[self.stops], -self.x[self.stops], c="blue", linewidth=1, linestyle="--")
-            xy = [self.Get_XY(initial=False)[1], -self.Get_XY(initial=False)[0]]
-            xytext = xy[0] - 0.05, xy[1] - 0.1
-            ax.annotate("END", xy=xy, xytext=xytext, weight="bold")
+            ax.plot(self.y_coordinates[self.visited_stops], -self.x_coordinates[self.visited_stops], c="blue", linewidth=1, linestyle="--")
+            coordinates = [self.Fetch_Coordinates(initial=False)[1], -self.Fetch_Coordinates(initial=False)[0]]
+            xytext = coordinates[0] - 0.05, coordinates[1] - 0.1
+            ax.annotate("END", xy=coordinates, xytext=xytext, weight="bold")
 
         ax.invert_yaxis()
 
@@ -89,49 +85,52 @@ class SamplingPointEnvironment(object):
         else:
             plt.show()
     
-    # Reset the environment by clearing all the stops and selecting a random stop as the first stop
-    def Reset(self):
-        self.stops = []
+    # Initialise the environment by clearing all the visited_stops and selecting the first stop as the first stop
+    def Initialise(self):
+        self.visited_stops = []
         first_stop = 0
-        self.stops.append(first_stop)
+        self.visited_stops.append(first_stop)
         return first_stop
 
-    # Step function that takes the destination as an input and returns the new state, reward and done
-    def Step(self, destination):
-        state = self.Get_State()
-        new_state = destination
-        reward = self.Get_Reward(state, new_state)
-        self.stops.append(destination)
-        done = len(self.stops) == self.n_stops
+    # Take_Step function that takes the destination as an input and returns the new current_state, reward and done
+    '''
+    This function is called in run_episode taking as an argument the action chosen by the agent.
+    '''
+    def Take_Step(self, destination):
+        current_state = self.Current_State()
+        next_state = destination
+        reward = self.Calculate_Reward(current_state, next_state)
+        self.visited_stops.append(destination)
+        done = len(self.visited_stops) == self.stop_count # if the number of visited_stops is equal to the number of visited_stops in the environment, then the episode is done
 
-        return new_state, reward, done
+        return next_state, reward, done
     
-    # returns the most recently bisited stop's index which is the current state of the environment
-    def Get_State(self):
-        return self.stops[-1]
+    # returns the most recently visited stop's index which is the current current_state of the environment
+    def Current_State(self):
+        return self.visited_stops[-1]
 
 
-    def Get_XY(self,initial = False):
-        state = self.stops[0] if initial else self.Get_State()
-        x = self.x[state]
-        y = self.y[state]
+    def Fetch_Coordinates(self,initial = False):
+        current_state = self.visited_stops[0] if initial else self.Current_State()
+        x = self.x_coordinates[current_state]
+        y = self.y_coordinates[current_state]
         return x,y
 
     # calculate the reward from moving from one stop to another
-    def Get_Reward(self, state, new_state):
-        base_reward = self.q_stops[state, new_state]
-        return base_reward
+    def Calculate_Reward(self, current_state, next_state):
+        reward_value = self.stop_distances[current_state, next_state]
+        return reward_value
 
 def run_episode(env,agent,verbose = 1):
 
-    # Reset the environment
-    s = env.Reset()
+    # Initialise the environment
+    s = env.Initialise()
 
-    # Reset the agent memory
-    agent.Reset_Memory()
+    # Initialise the agent memory
+    agent.Initialise_Memory()
 
-    # Get the maximum number of stops
-    max_Step = env.n_stops
+    # Get the maximum number of visited_stops
+    max_Take_Step = env.stop_count
     
     # Initialize the episode reward
     episode_reward = 0
@@ -140,7 +139,7 @@ def run_episode(env,agent,verbose = 1):
     total_distance = 0
     
     i = 0
-    while i < max_Step:
+    while i < max_Take_Step:
 
         # Remember the states
         agent.Remember_State(s)
@@ -149,7 +148,7 @@ def run_episode(env,agent,verbose = 1):
         a = agent.act(s)
         
         # Take the action, and get the reward from environment
-        s_next,r,done = env.Step(a)
+        s_next,r,done = env.Take_Step(a)
 
         # Tweak the reward
         r = -1 * r
@@ -177,10 +176,10 @@ class SamplingPointQAgent(QAgent):
 
     def __init__(self,*args,**kwargs):
         super().__init__(*args,**kwargs)
-        self.Reset_Memory()
+        self.Initialise_Memory()
 
     '''
-    Does take into account the current state but also considers the states already visited by the agent.
+    Does take into account the current current_state but also considers the states already visited by the agent.
     The agent avoids revisiting the states that it has already visited by setting their Q-value to -inf.
     '''
     def act(self,s):
@@ -203,10 +202,10 @@ class SamplingPointQAgent(QAgent):
         self.states_memory.append(s)
 
     # it allows the agent to forget the states that it has visited and therefore refresh its memory
-    def Reset_Memory(self):
+    def Initialise_Memory(self):
         self.states_memory = []
 
-def run_n_episodes(env,agent,name="training.gif",n_episodes=2000,Render_each=100,fps=10):
+def run_n_episodes(env,agent,name="training.gif",n_episodes=2000,Render_each=100):
 
     # Store the rewards
     rewards = []
@@ -217,6 +216,7 @@ def run_n_episodes(env,agent,name="training.gif",n_episodes=2000,Render_each=100
     start_time = time.time()
 
     # Experience replay
+    # the tqdm_notebook is used to show the progress bar to see our progress within the loop
     for i in tqdm_notebook(range(n_episodes)):
 
         # Run the episode
